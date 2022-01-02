e708db9e445ad50b05476fe8ed827dba4355d6de
Mugumya Vicent <mugumyavicent21@gmail.com>
2022-01-02 01:43:57 -0800

    email_notify

---
+++ desktop_notification.py
@@ -0,0 +1,10 @@
+from plyer import notification
+
+notification.notify(
+         title = "e-mail notification",
+         message = "you have 3 unread messages",
+         timeout = 10,
+   )
+
+
+
